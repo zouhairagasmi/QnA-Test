bb3c966dec14bf1405e9505fe4daccdb52133ce5
zouhairagasmi <Zouhaira.gasmi@esprit.tn>
2020-11-21 19:45:40 +0100

    cm

--- qna_solution.py
+++ qna_solution.py
@@ -1,15 +1,18 @@
-#import regex
+# Python program to find the 10 most frequent words 
+# from a text file
+
+
 import re
-#import collections
 import collections
-#read text file
+
+#Read input file
 with open('Tempest.txt') as f:
     text = f.read()
 
-#find all the words in the text file
+#retrieve the words in the text file using regex
 words = re.compile(r"[\w']+", re.U).findall(text.lower())
-#printing the most frequent words
-print('most 10 common words on this file are :')
+#counting the each word's occurence and printing the 10 most frequent words using collections counter
+print('The 10 most common words in this file are as follows: ')
 i = 1
 for value, count in collections.Counter(words).most_common(10):
     print(str(i)+'. ', value, '(' +str(count)+')')
