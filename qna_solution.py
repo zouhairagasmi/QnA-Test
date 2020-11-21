76964fe07e733c2c03775705558c24052830de24
zouhairagasmi <Zouhaira.gasmi@esprit.tn>
2020-11-21 18:50:30 +0100

    solution

--- qna_solution.py
+++ qna_solution.py
@@ -1,10 +1,16 @@
+#import regex
 import re
+#import collections
 import collections
-
+#read text file
 with open('Tempest.txt') as f:
     text = f.read()
 
-
+#find all the words in the text file
 words = re.compile(r"[\w']+", re.U).findall(text.lower())
-most_common = collections.Counter(words).most_common(10)
-print(most_common)
+#printing the most frequent words
+print('most 10 common words on this file are :')
+i = 1
+for value, count in collections.Counter(words).most_common(10):
+    print(str(i)+'. ', value, '(' +str(count)+')')
+    i += 1
